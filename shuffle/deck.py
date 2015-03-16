2a4dc333ea308bc4a26cda93e47c816c93cfa5bd
Samuel Stewart <samuel@Samuels-MacBook-Pro.local>
2015-03-16 23:13:30 +1300

    Write chi squared function, fix typo in RandomShuffleStrategy class.

---
+++ shuffle/deck.py
@@ -0,0 +1,24 @@
+import card
+import random
+
+class Deck:
+    
+    def __init__(self, strategy):
+        self.cards = []
+        self.strategy = strategy
+       
+        for suit in range(1, 5):
+
+            for val in range(2, 11):
+                self.cards.append(card.Card(val,suit))
+
+            for val in ['Ace', 'King', 'Queen', 'Jack']:
+                self.cards.append(card.Card(val, suit))
+
+    def get_card(self):
+        return self.cards.pop()
+
+    def shuffle(self):
+        self.strategy.shuffle(self.cards)
+                
+
---
+++ shuffle/shuffle.py
@@ -0,0 +1,28 @@
+from math import pow
+from shufflestrategy import RandomShuffleStrategy, AppendShuffleStrategy, EvenOddShuffleStrategy
+from deck import Deck
+
+
+strats_dict = {"Random class shuffle": Deck(RandomShuffleStrategy()),
+          "Append shuffle" : Deck(AppendShuffleStrategy()),
+          "Even odd shuffle strat" : Deck(EvenOddShuffleStrategy())}
+
+def build_dict(deck):
+    res_dict = {str(x):[0]*52 for x in deck.cards}
+    for i in range(520):
+        deck.shuffle()
+        for i in range(len(deck.cards)):
+            res_dict[str(deck.cards[i])][i] += 1
+    return res_dict
+
+def chi_square(res_dict):
+    res = 0
+    for li in res_dict.itervalues():
+        for i in range(0, len(li)):
+            res += pow((li[i] - 10), 2) / 10
+    return res
+
+for strat_name, deck in strats_dict.iteritems():
+    print strat_name + ": " + str(chi_square(build_dict(deck)))
+
+
