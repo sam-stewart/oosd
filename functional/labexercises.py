e6379ac2f3f23cb148641af740ae211b3e72b497
Samuel Stewart <stewasc3@student.op.ac.nz>
2015-04-22 22:01:51 +1200

    Added recursive map function and some tests

--- functional/labexercises.py
+++ functional/labexercises.py
@@ -1,47 +1,35 @@
 # Lambda expression exercises (lab 8.1)
+# File conventions: c is a character, st is a string, l is a list.
 
-# Computes factorials
-reduce(lambda x, y : x * y, range(1, 6))
+#1 Computes factorials from a list
+def factorial(l):
+    return reduce(lambda x, y : x * y, l)
 
-# Function takes a string, and a char, returns new string without chars.
+#2 Function takes a string, and a char, returns new string without chars.
 remove_char = lambda c, st : filter(lambda x : x != c, st)
-remove_char('c', 'christchurch')
 
-# Returns occurences of char in string
-len(filter(lambda x : x != 'c', 'christchurch'))
-reduce(lambda x, y: x + 1 if y == 'c' else x + 0, 'christchurch', 0)
+#3 Returns number of occurences of char in string
+def occurences(c, st):
+    return len(filter(lambda x : x != c, st))
 
-# Take string and char, return the number of words starting with char
-st = """Never drive Vin Diesel's car. Vin Diesel's car has twenty-six gears. 
-    It is much too difficult"""
-c = 'c'
-
+#4 Counts words in string beginning with char c.
 def count_words(c, st):
     return len(filter(lambda x : x.startswith(c), st.split()))
-print str(count_words(c, st))
 
-# Take string and char, returns new string with all occurences of char uppercase.
-# This is retarded, st.replace is all that is needed.
-#to_upper = lambda c, st : st.replace(c, c.upper)
-#print to_upper(c, st)
-
-# Version of map() using a loop
-def mymap(f):
+#6 version of map() using a loop (map takes a function and a list)
+def mymap_loop(f, l):
     retlist = []
-    for i in range(1,10):
+    for i in l:
         retlist.append(f(i))
     return retlist
 
-print mymap(lambda x : x * 2)
+#7 Version of map() using recursion (map takes a function and a list)
+def mymap_recursive(f, l):
+    if not l:
+        return [] 
+    return [f(l[0])] + mymap_recursive(f, l[1:])
 
-# Version of map() using recursion
-def mymapr(l, retlist = []):
-    retlist.append(2 * l.pop())
-    if len(l) > 0:
-        mymapr(l, retlist)
-    return retlist
-print(mymapr(range(1,10)))
-
+    
 
 
     
