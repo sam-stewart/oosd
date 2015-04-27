9847070da0b142a0d8048531f46fc941cdc20bfb
Samuel Stewart <stewasc3@student.op.ac.nz>
2015-04-28 02:39:01 +1200

    Recursion sillys.

--- functional/closures_generators.py
+++ functional/closures_generators.py
@@ -1,22 +1,26 @@
-# Keeps full series in a list
-def fib(n):
-    l = [0,1]
-    while(len(l) < n):
-        l.append(l[-1] + l[-2])
-    return l[n-1]
+from math import sqrt
+
+# Formula from wolfram alpha
+def fib_cheat(n):
+    return ((1 + sqrt(5))**n - (1 - sqrt(5))**n) / (2**n * sqrt(5))
+
+# Recursion play.
+def fib(n, a=0, b=1, c=0):
+    if c >= n:
+        return a
+    return fib(n, b, b+a, c+1)
 
 def fib_memo():
     l = [0,1]
     def fib(n):
         try:
-            return l[n-1]
+            return l[n]
         except IndexError:
             l.append(l[-1] + l[-2])
             return fib(n)
     return fib
 
 fib_closure = fib_memo()
-print fib_closure(5)
-print fib_closure(300)
 
-print fib3(5)
+print fib_closure(5)
+print fib(5)
