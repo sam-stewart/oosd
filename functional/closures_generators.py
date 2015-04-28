51b1181717522d427892d5845cc4ac2744e28306
Samuel Stewart <stewasc3@student.op.ac.nz>
2015-04-28 14:39:18 +1200

    Refactor prime test

--- functional/closures_generators.py
+++ functional/closures_generators.py
@@ -45,7 +45,7 @@ def is_prime(n):
         return False
     if n == 1:
         return True
-    for i in range (2, int(sqrt(n)+1)):
+    for i in range (2, int(sqrt(n))):
         if n % i == 0:
             return False
     return True
